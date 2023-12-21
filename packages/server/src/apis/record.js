0fbae47d925c2e44e02e047feaa897fbd1db070e
SepVeneto <sepveneto@qq.com>
2023-12-21 14:40:50 +0800

    feat(server): remove record sign check

--- packages/server/src/apis/record.js
+++ packages/server/src/apis/record.js
@@ -2,23 +2,21 @@ const Router = require('@koa/router')
 const router = new Router()
 const db = require('../db')
 const { ObjectId } = require('mongodb')
-const { sign } = require('sign')
 
 router.post('/record', async (ctx, next) => {
-  const { sign: signStr, appid, ...params } = ctx.request.body
-  const app = await db.collection('apps').findOne({ _id: new ObjectId(appid) })
-
-  const signRes = sign(JSON.stringify({ ...params, appid}), app.secret)
-  if (signRes === signStr) {
-    const logs = db.collection('logs')
-    const { sign, nonce, ...params } = ctx.request.body
-    await logs.insertOne(params)
-    await next()
-  } else {
+  const { appid, ...params } = ctx.request.body
+  if (!appid) {
     await next()
     ctx.body.code = 1
-    ctx.body.message = '非法请求'
+    ctx.body.message = 'missing appid'
+    return
   }
+  const app = await db.collection('apps').findOne({ _id: new ObjectId(appid) })
+
+  const logs = db.collection('logs')
+  const params = ctx.request.body
+  await logs.insertOne(params)
+  await next()
 })
 
 module.exports = router
