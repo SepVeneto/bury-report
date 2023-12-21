7080583e1f38b0d0d344ce54d25af68efdfc7e7e
root <root@localhost.localdomain>
2023-12-21 09:32:55 +0800

    fix: docker link

--- packages/server/src/db.js
+++ packages/server/src/db.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb')
 
-// const client = new MongoClient('mongodb://127.0.0.1:27017')
+// const client = new MongoClient('mongodb://db:27017')
 const client = new MongoClient(`mongodb://${process.env.REPORT_DB_URL}`)
 
 client.connect()
