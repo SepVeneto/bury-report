096fdfcdb24c90dc613b26ba83a21ba53ced1bf1
SepVeneto <sepveneto@qq.com>
2023-12-27 08:44:29 +0800

    feat: db auth

--- packages/server/src/db.js
+++ packages/server/src/db.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb')
 
 // const client = new MongoClient('mongodb://db:27017')
-const client = new MongoClient(`mongodb://${process.env.REPORT_DB_URL}`)
+const client = new MongoClient(`mongodb://${process.env.DB_NAME}:${process.env.DB_PWD}@${process.env.REPORT_DB_URL}`)
 
 client.connect()
 
