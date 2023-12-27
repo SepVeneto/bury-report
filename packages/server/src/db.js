237dc967fef0896f705e1f432a4a0f8cc9a09723
SepVeneto <sepveneto@qq.com>
2023-12-27 15:05:24 +0800

    fix(server): db error

--- packages/server/src/db.js
+++ packages/server/src/db.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb')
 
 // const client = new MongoClient('mongodb://db:27017')
-const client = new MongoClient(`mongodb://${process.env.DB_NAME || 'admin'}:${process.env.DB_PWD || 'admin_123'}@${process.env.REPORT_DB_URL}`)
+const client = new MongoClient(`mongodb://${process.env.DB_NAME || 'root'}:${process.env.DB_PWD || 'root_123'}@${process.env.REPORT_DB_URL}`)
 
 const db = client.db('reporter')
 
@@ -11,14 +11,22 @@ module.exports = db
 
 async function initDb() {
   await client.connect()
-  db.createCollection('captcha')
-  db.createCollection('apps')
-  db.createCollection('projects')
-  db.createCollection('users')
-  db.createCollection('logs')
+  const collections = await db.listCollections().toArray()
+  createCollection('captcha', collections)
+  createCollection('apps', collections)
+  createCollection('projects', collections)
+  createCollection('users', collections)
+  createCollection('logs', collections)
   const captcha = db.collection('captcha')
   const exist = (await captcha.indexes()).find(index => index.name === 'createTime')
   if (!exist) {
     captcha.createIndex({ 'createTime': 1 }, { name: 'createTime', expireAfterSeconds: 10 * 60 })
+  }
+}
+
+function createCollection(name, collections) {
+  const exist = collections.find(collection => collection.name === name)
+  if (!exist) {
+    db.createCollection(name)
   }
 }
