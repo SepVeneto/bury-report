9694d5ef04a2f1ea7dd1725e05f6c3a1fb946c51
SepVeneto <sepveneto@qq.com>
2024-09-06 13:36:31 +0800

    feat(core): record static resource error

--- packages/core/src/helper/error/browser.ts
+++ packages/core/src/helper/error/browser.ts
@@ -8,4 +8,31 @@ function reportContent(error: { name: string, message: string, stack?: string })
 
 export function __BR_ERROR_INIT__() {
   initErrorProxy(reportContent)
+
+  window.addEventListener('error', (evt?: ErrorEvent) => {
+    if (!evt) return
+
+    if (evt.target instanceof HTMLElement) {
+      if (evt.target instanceof HTMLScriptElement) {
+        reportContent({
+          name: 'JsLoadFail',
+          message: evt.target.src,
+          stack: evt.target.baseURI,
+        })
+      }
+      if (evt.target instanceof HTMLLinkElement) {
+        reportContent({
+          name: 'CssLoadFail',
+          message: evt.target.href,
+          stack: evt.target.baseURI,
+        })
+      }
+    } else {
+      reportContent({
+        name: evt.error?.name || 'unknown',
+        message: evt.error?.message || evt.message,
+        stack: evt.error?.stack || `${evt.filename}:${evt.lineno},${evt.colno}`,
+      })
+    }
+  }, true)
 }
