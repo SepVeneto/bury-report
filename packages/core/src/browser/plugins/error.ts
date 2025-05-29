363b5b2582d4db11f027ca564e113504fe8ef276
SepVeneto <sepveneto@qq.com>
2025-05-29 16:56:34 +0800

    fix(mp): catch custom error

--- packages/core/src/browser/plugins/error.ts
+++ packages/core/src/browser/plugins/error.ts
@@ -84,40 +84,44 @@ export class ErrorPlugin implements BuryReportPlugin {
 
 function initErrorProxy(reportFn: (...args: any[]) => void) {
   const _tempError = console.error
-  console.error = function (...args) {
-    for (const arg of args) {
-      if (typeof arg === 'string') {
-        const error = {
-          name: 'CustomError',
-          message: arg,
-          stack: '',
+  try {
+    console.error = function (...args) {
+      for (const arg of args) {
+        if (typeof arg === 'string') {
+          const error = {
+            name: 'CustomError',
+            message: arg,
+            stack: '',
+          }
+          reportFn(error)
+          break
         }
-        reportFn(error)
-        break
-      }
-      if (arg instanceof Error) {
-        const error = {
-          name: arg.name,
-          message: arg.message,
-          stack: arg.stack,
+        if (arg instanceof Error) {
+          const error = {
+            name: arg.name,
+            message: arg.message,
+            stack: arg.stack,
+          }
+          reportFn(error)
+          break
         }
-        reportFn(error)
-        break
-      }
-      if (globalThis.PromiseRejectionEvent && arg instanceof PromiseRejectionEvent && arg.reason) {
-        const error = {
-          name: arg.reason.name,
-          message: arg.reason.message,
-          stack: arg.reason.stack,
+        if (globalThis.PromiseRejectionEvent && arg instanceof PromiseRejectionEvent && arg.reason) {
+          const error = {
+            name: arg.reason.name,
+            message: arg.reason.message,
+            stack: arg.reason.stack,
+          }
+          reportFn(error)
+          break
+        } else {
+          console.warn(args)
+          console.warn(arg, typeof arg, Object.prototype.toString.call(arg))
         }
-        reportFn(error)
-        break
-      } else {
-        console.warn(args)
-        console.warn(arg, typeof arg, Object.prototype.toString.call(arg))
       }
+      _tempError.apply(this, args)
     }
-    _tempError.apply(this, args)
+  } catch (err) {
+    console.warn(err)
   }
   return _tempError
 }
