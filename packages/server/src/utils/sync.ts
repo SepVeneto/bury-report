37ada559ca574d362a009f4f811f5a8775138e19
SepVeneto <sepveneto@qq.com>
2026-03-02 10:33:42 +0800

    chore(server): limit duckdb

--- packages/server/src/utils/sync.ts
+++ packages/server/src/utils/sync.ts
@@ -19,7 +19,7 @@ export async function init(apps: string[]) {
     dbMap.set(app, instance)
     const conn = await instance.connect()
     connMap.set(app, conn)
-    await conn.run(`PRAGMA memory_limit='2.5GB';`);
+    await conn.run(`PRAGMA memory_limit='2GB';`);
 
     await conn.run(`
       CREATE TABLE IF NOT EXISTS network_recent(
@@ -72,7 +72,7 @@ export async function getConn(dbName: string) {
       dbMap.set(dbName, instance)
     }
     conn = await instance.connect()
-    await conn.run(`PRAGMA memory_limit='2.5GB';`);
+    await conn.run(`PRAGMA memory_limit='2GB';`);
     console.warn(`${dbName}数据库未连接`)
     connMap.set(dbName, conn)
   }
